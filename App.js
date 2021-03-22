246f2755a5c1944defcbe5b1b16f5332092de437
sandrayousry <77549291+sandrayousry@users.noreply.github.com>
2021-03-22 13:07:22 +0200

    lab2 todolist
    lab2 todolist

--- App.js
+++ App.js
@@ -1,40 +1,96 @@
-// import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import './App.css';
+import ListItems from './ListItems'
+
+
 class App extends React.Component{
-constructor(){
-  super();
-  this.state ={
-    title:"Welcome",
-    count:0
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentitem:{
+        text:'',
+        key:''
+      }
+
+    }
+    this.handelinput = this.handelinput.bind(this);
+    this.additem = this.additem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  
+
+
   }
-  this.changeCounter=this.changeCounter.bind(this);
-}
-changeCounter(){
-// this.state.title ="sandra"
-// this.setState({title:"sandra"});
-let newCount = this.state.count+1;
-this.setState({count:newCount});
-}
-render(){
+  handelinput(e){
+    this.setState({
+      currentitem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  // to prevent page refresh after clicking on add button
+  additem(e){
+    e.preventDefault();
+    const newitem = this.state.currentitem;
+    // console.log(newitem);
+    // add item
+    // check if newitem not empty
+    if(newitem.text!==""){
+      // destructuring assignment format
+       const newitems = [...this.state.items, newitem];
+       this.setState({
+         items:newitems,
+         currentitem:{
+           text:'',
+           key:''
+         }
+       })
+      }
+    }
+    deleteItem(key){
+      const filteredItems= this.state.items.filter(item =>
+        item.key!==key);
+      this.setState({
+        items: filteredItems
+      })
+  
+    }
+
+    setUpdate(text,key){
+      
+      const items = this.state.items;
+      items.map(item=>{      
+        if(item.key===key){
+          item.text= text;
+        }
+      })
+      this.setState({
+        items: items
+      })
+    }
+
+
+
+
+  
+  render(){
   return (
-    <div style={{marginLeft:"500px"}}>
-      <div>
-        <h3 style={{color:"red"}} >click on the button to increase the counter</h3>
-        <h1>{this.state.count}</h1>
-      </div>
-      <button onClick={this.changeCounter}>changeCounter</button>
+    <div className="app">
+    <form id="todoform" onSubmit={this.additem}>
+      <input type="text" placeholder="Enter text" value={this.state.currentitem.text} 
+      onChange={this.handelinput}/>
+      <button type="submit">Add</button>  
+    </form>
+           
+
+
+      <ListItems items = {this.state.items} deleteItem ={this.deleteItem} setUpdate={this.setUpdate} ></ListItems>    
+   
     </div>
+ 
   );
 }
 }
-
 export default App;
-
-
-
-
-
-
-
-
